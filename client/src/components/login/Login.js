cac87660d1d560eaebdc707c8439b7bbd55b75c7
ajcudd <ajcuddeback@gmail.com>
2021-04-01 16:08:39 -0400

    configured form state

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -8,6 +8,16 @@ function Login() {
     const [signupData, setSignupData] = useState({username: '', email: '', first_name: '', last_name: '', password: ''});
     const [isNewUser, setIsNewUser] = useState(false);
 
+    const handleLoginChange = (e) => {
+        const { name, value } = e.target;
+        setUserLoginData({ ...userLoginData, [name]: value })
+    }
+
+    const handleSignupChange = (e) => {
+        const { name, value } = e.target;
+        setSignupData({ ...signupData, [name]: value })
+    }
+
     // JSX
     if(!isNewUser) {
         return (
@@ -17,9 +27,9 @@ function Login() {
                 </div>
                 <form className="login-form">
                     <h2>Login</h2>
-                    <input type="text" name="username" id="username" placeholder="Username"/>
+                    <input type="text" name="username" id="username" placeholder="Username" onChange={handleLoginChange} />
                     <br/>
-                    <input type="password" name="password" id="password" placeholder="Password"/>
+                    <input type="password" name="password" id="password" placeholder="Password"onChange={handleLoginChange} />
                     <br/>
                     <p className="errorP"></p>
                     <button type="submit">Login</button>
@@ -35,15 +45,15 @@ function Login() {
                 </div>
                 <form className="login-form">
                     <h2>Signup</h2>
-                    <input type="text" name="username" id="username" placeholder="Username"/>
+                    <input type="text" name="username" id="username" placeholder="Username" onChange={handleSignupChange} />
                     <br/>
-                    <input type="email" name="email" id="email" placeholder="example@example.com"/>
+                    <input type="email" name="email" id="email" placeholder="example@example.com" onChange={handleSignupChange} />
                     <br/>
-                    <input type="text" name="first_name" id="firstName" placeholder="First Name"/>
+                    <input type="text" name="first_name" id="firstName" placeholder="First Name" onChange={handleSignupChange} />
                     <br/>
-                    <input type="text" name="last_name" id="lastName" placeholder="Last Name"/>
+                    <input type="text" name="last_name" id="lastName" placeholder="Last Name" onChange={handleSignupChange} />
                     <br/>
-                    <input type="password" name="password" id="password" placeholder="Password"/>
+                    <input type="password" name="password" id="password" placeholder="Password" onChange={handleSignupChange} />
                     <br/>
                     <p className="errorP"></p>
                     <button type="submit">Login</button>
