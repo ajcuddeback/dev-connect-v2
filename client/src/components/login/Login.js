e1b4d0e3869f362b4fc9c75708ac4bb9e7ec6a7f
ajcudd <ajcuddeback@gmail.com>
2021-04-01 16:02:32 -0400

    added layout for login and signup

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -13,10 +13,41 @@ function Login() {
         return (
             <>
                 <div className="img-wrapper">
-                    <img src={logo} alt="dev-connect logo" className="logo"/>
+                    <img src={logo} alt="dev-connect logo" className="logo" width="500px"/>
                 </div>
                 <form className="login-form">
                     <h2>Login</h2>
+                    <input type="text" name="username" id="username" placeholder="Username"/>
+                    <br/>
+                    <input type="password" name="password" id="password" placeholder="Password"/>
+                    <br/>
+                    <p className="errorP"></p>
+                    <button type="submit">Login</button>
+                    <p>Dont have an account with us? <span onClick={() => setIsNewUser(!isNewUser)}>Signup here!</span></p>
+                </form>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <div className="img-wrapper">
+                    <img src={logo} alt="dev-connect logo" className="logo" width="500px"/>
+                </div>
+                <form className="login-form">
+                    <h2>Signup</h2>
+                    <input type="text" name="username" id="username" placeholder="Username"/>
+                    <br/>
+                    <input type="email" name="email" id="email" placeholder="example@example.com"/>
+                    <br/>
+                    <input type="text" name="first_name" id="firstName" placeholder="First Name"/>
+                    <br/>
+                    <input type="text" name="last_name" id="lastName" placeholder="Last Name"/>
+                    <br/>
+                    <input type="password" name="password" id="password" placeholder="Password"/>
+                    <br/>
+                    <p className="errorP"></p>
+                    <button type="submit">Login</button>
+                    <p>Already have an account? <span onClick={() => setIsNewUser(!isNewUser)}>Signup here!</span></p>
                 </form>
             </>
         )
