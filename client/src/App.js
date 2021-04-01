6f230878944d5ae86061bb1a47af7c2e2c3a11c5
ajcudd <ajcuddeback@gmail.com>
2021-04-01 15:35:00 -0400

    added apollo dependencies and set up app.js with apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
+import React from 'react';
+
+// Apollo
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}`: ''
+      }
+    })
+  }, 
+  uri: '/graphql'
+})
+
 function App() {
   return (
-    <div className="App">
+    <ApolloProvider client={client}>
       <h2>Hello React!</h2>
-    </div>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/components/login/Login.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+    return (
+        <h2>This is the login screen</h2>
+    )
+}
