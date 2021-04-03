2c42f4f8658a6d98fd6c2a7c4d2535b25a744941
ajcudd <ajcuddeback@gmail.com>
2021-04-03 12:18:50 -0400

    cleaned code

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,14 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
+// Components
 import Login from './components/login/Login';
 import Nav from './components/layout-components/Nav';
 
+// Utils
 import Auth from './utils/auth'
 
+// Client
 const client = new ApolloClient({
   request: operation => {
     const token = localStorage.getItem('id_token');
