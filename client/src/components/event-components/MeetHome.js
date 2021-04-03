148e494af5e7e9294aaab81a1f59dc2204ba40fc
ajcudd <ajcuddeback@gmail.com>
2021-04-03 19:32:12 -0400

    added zip code state and functions

--- client/src/components/event-components/MeetHome.js
+++ client/src/components/event-components/MeetHome.js
@@ -3,10 +3,23 @@ import React, { useState } from 'react';
 const MeetHome = () => {
     // State
     const [miles, setMiles] = useState(20);
+    const [zipCode, setZipCode] = useState();
 
     // Functions
     const handleRange = (event) => {
         setMiles(event.target.value)
+    };
+
+    const handleZipChange = (e) => {
+        setZipCode(e.target.value)
+    };
+
+    const findGroupHandler = (e) => {
+        e.preventDefault();
+    };
+
+    const findGroupZipHandler = () => {
+
     };
 
     // JSX
@@ -21,22 +34,22 @@ const MeetHome = () => {
             <section className="get-group-wrapper">
                 <div className="start-group-wrapper">
                     <form className="create-group-form">
-                        <input type="text" name="group-name" className="group-name" placeholder="Name of your group" />
+                        <input type="text" name="group-name" className="group-name" placeholder="Name of your group" required />
                         <textarea name="about-group" id="about-group" cols="30" rows="10"
-                            placeholder="What is your group about?"></textarea>
-                        <input type="number" name="zip" className="zip" placeholder="Zip Code" />
+                            placeholder="What is your group about?" required></textarea>
+                        <input type="number" name="zip" className="zip" placeholder="Zip Code" required />
                         <button type="submit" className="grey-red-btn">Create Group</button>
                     </form>
                 </div>
                 <div className="find-group-wrapper">
                     <h2>Find a group near you!</h2>
-                    <form className="find-group-form">
+                    <form onSubmit={findGroupHandler} className="find-group-form">
                         <input type="range" className="mile-slider" max="150" min="10" value={miles} onChange={handleRange} />
                         <p>Within <span className="miles-nr">{miles}</span> miles</p>
-                        <input type="number" name="zip-code" placeholder="Zip Code" />
+                        <input onChange={handleZipChange} type="number" name="zip-code" placeholder="Zip Code" required />
                         <button type="submit" className="grey-red-btn">Find a group!</button>
                     </form>
-                    <button className="use-location grey-red-btn">Use your current location?</button>
+                    <button className="use-location grey-red-btn" onClick={findGroupZipHandler}>Use your current location?</button>
                     </div>
             </section>
         </>
