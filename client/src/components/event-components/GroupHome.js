f8cedd9ae780bd5234f76541b4e32fa65c1dd59d
ajcudd <ajcuddeback@gmail.com>
2021-04-05 09:51:34 -0400

    added error handling for if the group url does not exists

--- client/src/components/event-components/GroupHome.js
+++ client/src/components/event-components/GroupHome.js
@@ -9,8 +9,12 @@ import { GET_GROUP } from '../../utils/queries';
 
 import Auth from '../../utils/auth';
 
+import { Link } from 'react-router-dom';
+
 const GroupHome = () => {
     const [isMember, setIsMemeber] = useState(false);
+    const [isAttending, setIsAttending] = useState({});
+    const [dataGroup, setDataGroup] = useState(false);
 
     // Get the groupName from params
     let { groupName } = useParams();
@@ -21,24 +25,59 @@ const GroupHome = () => {
 
     useEffect(() => {
         if(data) {
+            if(data.group === null) {
+                return;
+            }
             const groupUsers = data.group.group_user;
             groupUsers.forEach(user => {
                 const userid = Auth.getProfile().data.id;
                 
                 if(parseInt(user.id) === userid) {
-                    console.log('yes')
                     setIsMemeber(true);
                 } else {
                     setIsMemeber(false);
-                    console.log('no')
                 }
-            })
+            });
+            setDataGroup(true);
         }
     }, [data])
 
+    if(loading) {
+        return (
+            <h2>Loading...</h2>
+        )
+    };
+
+    if(dataGroup === false) {
+        return(
+            <h2>No group exists at this url! Go back to the <Link to={`/meet`}>events page</Link>  to find a group in your area!</h2>
+        )
+    }
 
     return (
-        <h2>Group Home</h2>
+        <>
+            <h2>Group Home</h2>
+            <div className=" hidden attend-event-success">
+                <p>You are now successfully attending this event!</p>
+            </div>
+            <div className="hidden attend-event-fail">
+                <p>You are already attending this event!</p>
+            </div>
+
+            <section className="group-content-wrapper">
+                <div className="group-info-wrapper">
+                    <h2 className="group-name">{data.group.group_title}</h2>
+                    <p></p>
+                </div>
+                <div className="group-event-wrapper">
+                    <ol>
+                        <li>
+                           
+                        </li>
+                    </ol>
+                </div>
+            </section>
+        </>
     )
 };
 
