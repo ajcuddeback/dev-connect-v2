08a948b7fd365a414f08548e3106908b7a98c568
ajcudd <ajcuddeback@gmail.com>
2021-04-06 14:16:18 -0400

    added reset and glass

--- client/src/components/GlobalStyles.js
+++ client/src/components/GlobalStyles.js
@@ -7,16 +7,43 @@ const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
     }
 
+    html:focus-within {
+        scroll-behavior: smooth;
+    }
+
     body {
+        text-rendering: optimizeSpeed;
         min-height: 100vh;
         background: linear-gradient( #0f0718,  #151BC2);
     }
 
-    .glass-background {
+    button {
+        cursor: pointer;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    ol, ul {
+	    list-style: none;
+    }
+
+    .glass-background,
+    .glass-button {
         border: none;
         border-radius: 5px;
         background: rgba(255,255,255,0.35);
         backdrop-filter: blur(5px);
+    }
+
+    .glass-button {
+        padding: .5rem 1rem;
+        position: relative;
+        top: 0;
+    }
+    .glass-button:hover {
+        top: 3px;
     }
 `
 
