3d65897fe83d2d61984e0e1617bd25fc582131c6
jasmineakbari <jasmineakbari@yahoo.com>
2021-04-08 20:20:56 -0400

    feature/graphql-friends

---
+++ client/src/components/question-components/askeDevsHome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const askDevsHome = ({ questions, title }) => {
+    if (!questions.length) {
+        return <h3>No Questions Yet</h3>;
+    }
+
+    return(
+        <div>
+            
+        </div>
+    );
+};
+
+export default askDevsHome;
